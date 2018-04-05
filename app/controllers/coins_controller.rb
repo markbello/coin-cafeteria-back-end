eaa144bde5710666027c2c9c70f767d41dbbdd0d
markbello <mcbello51286@gmail.com>
2018-04-05 17:42:53 -0400

    Allow CORS for API

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,5 +1,7 @@
 class CoinsController < ApplicationController
 
+  after_action :update_coins
+
   def index
     @coins = Coin.all
     render json:@coins
@@ -28,6 +30,34 @@ class CoinsController < ApplicationController
   end
 
   private
+
+  def update_coins
+    top100_raw = open("https://api.coinmarketcap.com/v1/ticker/").read
+    top100= JSON.parse(top100_raw)
+
+    top100.each do |coin|
+      newCoin = Coin.find_or_create_by(symbol: coin["symbol"])
+      newCoin.coin_market_cap_id = coin["id"]
+      newCoin.name = coin["name"]
+      newCoin.symbol = coin["symbol"]
+      newCoin.rank = coin["rank"]
+      newCoin.price_usd= coin["price_usd"]
+      newCoin.price_btc= coin["price_btc"]
+      newCoin.volume_usd= coin["24h_volume_usd"]
+      newCoin.market_cap_usd= coin["market_cap_usd"]
+      newCoin.available_supply= coin["available_supply"]
+      newCoin.total_supply= coin["total_supply"]
+      newCoin.max_supply= coin["max_supply"]
+      newCoin.percent_change_1h= coin["percent_change_1h"]
+      newCoin.percent_change_24h= coin["percent_change_24h"]
+      newCoin.percent_change_7d= coin["percent_change_7d"]
+      newCoin.last_updated= coin["last_updated"]
+      puts coin["last_updated"]
+      newCoin.save
+      puts newCoin
+      # puts JSON.parse(coin)
+    end
+  end
 
   def coin_params
     params.require(:coin).permit(:symbol)
